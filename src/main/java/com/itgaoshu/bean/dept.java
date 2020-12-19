57b142491a10ed05df17ff5edc99cca5ea9d25f5
Tan30123 <3012388504@qq.com>
2020-12-19 15:07:57 +0800

    新增实体类

---
+++ src/main/java/com/itgaoshu/bean/dept.java
@@ -0,0 +1,19 @@
+/*
+ * 版权所有(C): 山东高数科技有限公司，$today.fomat("yyyy"),所有权利保留
+ *
+ * 项目名 : springboot
+ * 文件名 : dept.java
+ * 模块说明 :
+ * 修改历史 :
+ * 2020-12-19-Tan-创建
+ */
+
+package com.itgaoshu.bean;
+
+/**
+ * @author Tan
+ * @create 2020-12-19 15:07
+ */
+public class dept {
+    private  int id;
+}
